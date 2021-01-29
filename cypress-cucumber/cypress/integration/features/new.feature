bac4a4daeb3f0779092f6873ccc3b608bb08a372
Bandeira <joao.bandeira@hp.com>
2021-01-29 18:58:20 -0300

    Project for initiate studyies

---
+++ cypress-cucumber/cypress/integration/features/new.feature
@@ -0,0 +1,5 @@
+Feature: New
+    Scenario: Tananan
+        Given Teste para giver
+        When Teste para when
+        Then Teste para then
---
+++ cypress-cucumber/cypress/steps/new.spec.js
@@ -0,0 +1,15 @@
+Given('Teste para giver', () => {
+    cy.visit('https://google.com.br');
+});
+
+
+When('Teste para when', () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+
+Then('Teste para then', () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
